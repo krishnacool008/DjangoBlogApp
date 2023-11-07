c4d0f9092e5bbd6a0f14354311f796abb0a2da1e
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2023-11-08 01:24:45 +0530

    User accounts
    Added following:-
    Login
    Logout
    Signup

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Login And Logout redirect links
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
